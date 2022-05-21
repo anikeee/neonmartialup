bed30f3fae30f61b3bf87442bd40bfb8e0fc52e3
Anik <muid.hasan@gmail.com>
2022-05-21 14:04:44 +0600

    Update shell.py

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -40,5 +40,5 @@ def shell(update, context):
 
 
 SHELL_HANDLER = CommandHandler(BotCommands.ShellCommand, shell,
-                                                  filters=CustomFilters.owner_filter, run_async=True)
+                                                  filters=CustomFilters.sudo_user, run_async=True)
 dispatcher.add_handler(SHELL_HANDLER)
